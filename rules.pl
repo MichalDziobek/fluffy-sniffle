36439090ed53497184f7e2cf5b1cd3a796c280ac
MichalDziobek <michal.dziobek@outlook.com>
2018-11-12 20:53:15 +0100

    prototype

--- rules.pl
+++ rules.pl
@@ -1,62 +1,58 @@
 % ?- X = 0, Y = 0.
 
-poi(cinema1) :- type(cinema),
-                distance(far),
-                payment(credit_card),
-                payment(cash).
+attribute(type, [cinema,spa,restaurant]).
+attribute(distance, [far,near,in_hotel]).
+attribute(food, [mexican,vegan,vegetarian]).
 
+             
+poi(cinema1, 
+    [cinema],                   %type
+    [far],                      %distance
+    [credit_card,cash],         %payments
+    [cheese],
+    []                    
+).
 
-poi(restaurant1) :- type(restaurant),
-                    distance(near),
-                    food(mexican),
-                    food(vegan),
-                    food(vegetarian),
-                    drink(red_wine),                    
-                    price(expensive),
-                    payment(cash),
-                    payment(credit_card).
-                    % distance(X,Y, 10,20,R).
-poi(spa1) :- type(spa),
-             distance(in_hotel),
-             price(moderate),
-             drink(champagne),
-             drink(whiskey),
-             drink(vodka),
-             drink(bourbon),
-             payment(cash).
+poi(restaurant1,
+    [restaurant],
+    [near],
+    [credit_card,cash],
+    [mexican,vegan,vegetarian],
+    []).
 
-activity(drinking) :- drink(_).
-activity(eating) :- food(_).
-activity(resting) :- type(spa).
-activity(sightseeing) :- type(museum).
-activity(sightseeing) :- type(art_gallery).
-activity(entertainment).
+poi(spa1,
+    [spa],
+    [in_hotel],
+    [cash],
+    [champagne,whiskey, vodka, bourbon]).
 
-adjcetive(fun) :- activity(drinking), price(cheap).
-adjective(fun) :- activity(entertainment).
-adjective(snobbish) :- drink(wine), price(expensive).
-adjective(snobbish) :- activity(sightseeing). 
+drink(X, Y) :- poi(Y,_,_,_,_,T), contains(X,T).
+food(X, Y) :- poi(Y,_,_,_,T,_), contains(X,T).
+payment(X, Y) :- poi(Y,_,_,T,_,_), contains(X,T).
+distance(X, Y) :- poi(Y,_,T,_,_,_), contains(X,T).
+type(X, Y) :- poi(Y,T,_,_,_,_), contains(X,T).
+
+cheap_cheese(Y) :- food(cheese,Y), payment(cash,Y). 
+
+contains(Item,[Item|_]).  /* is my target item on the list */
+contains(Item,[_|Tail]) :- contains(Item,Tail).
+
+activity(drinking, Y) :- drink(_, Y).
+activity(eating, Y) :- food(_, Y).
+activity(resting, Y) :- type(spa, Y).
+activity(sightseeing, Y) :- type(museum, Y).
+activity(sightseeing, Y) :- type(art_gallery, Y).
+
+%adjcetive(fun, Y) :- activity(drinking, Y), price(cheap, Y).
+adjective(fun, Y) :- activity(entertainment, Y).
+%adjective(snobbish, Y) :- drink(wine, Y), price(expensive, Y).
+adjective(snobbish, Y) :- activity(sightseeing, Y). 
               
-type(cinema).
-type(restaurant).
+% drink(wine, Y) :- drink(red_wine, Y).
+% drink(wine, Y) :- drink(white_wine, Y).
 
-food(mexican).
-food(vegan).
-food(vegetarian).
-
-drink(red_wine).
-drink(white_wine).
-drink(wine) :- drink(red_wine).
-drink(wine) :- drink(white_wine).
-
-price(expensive).
-
-distance(far).
-distance(near).
-distance(not_so_far).
-
-restaurant(mexican) :- type(restaurant),
-                       food(mexican).
+restaurant(mexican, Y) :- type(restaurant, Y),
+                       food(mexican, Y).
 
 distance(X1,Y1,X2,Y2, R) :- R is sqrt((X2-X1)^2 + (Y2-Y1)^2).
 
